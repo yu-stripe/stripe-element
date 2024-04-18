f5463a57dcdc71cfc724a340b02a80441aa8c783
Yu Kitazume <yu@stripe.com>
2024-04-18 10:45:50 +0900

    simple app

---
+++ public/checkout.js
@@ -0,0 +1,145 @@
+console.log("start stripe code inject");
+
+
+// This is your test publishable API key.
+const stripe = Stripe("pk_test_51NQQoIEzgtKktpOy7rP0KtN9n9zBguSzEPYRvjaGuDzcFek2N9gN69tikNBWtdb2Pq2FkkHYWks0qydQBcJEpu3i00SbjW9HWP");
+
+// The items the customer wants to buy
+const items = [{ id: "xl-tshirt" }];
+
+let elements;
+let selectedPaymentMethod;
+
+initialize();
+checkStatus();
+
+document
+  .querySelector("#c-form")
+  .addEventListener("submit", handleSubmit);
+
+
+// Fetches a payment intent and captures the client secret
+async function initialize() {
+  const response = await fetch("/create-payment-intent", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ items }),
+  });
+  const { clientSecret } = await response.json();
+
+  const appearance = {
+    theme: 'stripe',
+  };
+  const externalPaymentMethodTypes =  ['external_paypay', 'external_paidy', 'external_rakuten_pay'];
+  elements = stripe.elements({ appearance, clientSecret, externalPaymentMethodTypes });
+
+  const paymentElementOptions = {
+    layout: "tabs",
+  };
+
+  const paymentElement = elements.create("payment", paymentElementOptions);
+  paymentElement.mount("#payment-element");
+
+  paymentElement.on('change', (event) => {
+      selectedPaymentMethod = event?.value?.type;
+    console.log(selectedPaymentMethod);
+  });
+}
+
+async function handleSubmit(e) {
+  console.log("test")
+  console.log(selectedPaymentMethod);
+  e.preventDefault();
+  //setLoading(true);
+ if (selectedPaymentMethod === 'external_paypay') {
+
+   let totalAmount = "{{ orderTotal }}";
+   const response = await fetch('/api/create-paypay', {
+     method: 'POST',
+     headers: { 'Content-Type': 'application/json' },
+     body: JSON.stringify({
+       amount: totalAmount 
+     })
+   });
+   const { url } = await response.json();
+   const paypayRedirectUrl =  url;
+   window.location.href = paypayRedirectUrl;
+  }
+  const { error } = await stripe.confirmPayment({
+    elements,
+    confirmParams: {
+      // Make sure to change this to your payment completion page
+      return_url: "https://yu-demo.tunnel.stripe.me/",
+    },
+  });
+
+  // This point will only be reached if there is an immediate error when
+  // confirming the payment. Otherwise, your customer will be redirected to
+  // your `return_url`. For some payment methods like iDEAL, your customer will
+  // be redirected to an intermediate site first to authorize the payment, then
+  // redirected to the `return_url`.
+  if (error.type === "card_error" || error.type === "validation_error") {
+    showMessage(error.message);
+  } else {
+    showMessage("An unexpected error occurred.");
+  }
+
+  setLoading(false);
+}
+
+// Fetches the payment intent status after payment submission
+async function checkStatus() {
+  const clientSecret = new URLSearchParams(window.location.search).get(
+    "payment_intent_client_secret"
+  );
+
+  if (!clientSecret) {
+    return;
+  }
+
+  const { paymentIntent } = await stripe.retrievePaymentIntent(clientSecret);
+
+  switch (paymentIntent.status) {
+    case "succeeded":
+      showMessage("Payment succeeded!");
+      break;
+    case "processing":
+      showMessage("Your payment is processing.");
+      break;
+    case "requires_payment_method":
+      showMessage("Your payment was not successful, please try again.");
+      break;
+    default:
+      showMessage("Something went wrong.");
+      break;
+  }
+}
+
+// ------- UI helpers -------
+
+function showMessage(messageText) {
+  const messageContainer = document.querySelector("#payment-message");
+
+  messageContainer.classList.remove("hidden");
+  messageContainer.textContent = messageText;
+
+  setTimeout(function () {
+    messageContainer.classList.add("hidden");
+    messageContainer.textContent = "";
+  }, 4000);
+}
+
+// Show a spinner on payment submission
+function setLoading(isLoading) {
+  if (isLoading) {
+    // Disable the button and show a spinner
+    document.querySelector("#submit").disabled = true;
+    document.querySelector("#spinner").classList.remove("hidden");
+    document.querySelector("#button-text").classList.add("hidden");
+  } else {
+    document.querySelector("#submit").disabled = false;
+    document.querySelector("#spinner").classList.add("hidden");
+    document.querySelector("#button-text").classList.remove("hidden");
+  }
+}
+
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+var cors = require('cors');
+
+const path = require('path');
+
+console.log(process.env.PAYPAY_SECRET_KEY)
+router.use(cors());
+const paypayKey = process.env.PAYPAY_SECRET_KEY
+
+const PAYPAY = require('@paypayopa/paypayopa-sdk-node');
+const uuid = require('uuid');
+PAYPAY.Configure({
+  clientId: 'a_yeMLLbTuuy_EZNl',
+  clientSecret: paypayKey,
+  merchantId: "689533420559024128",
+  productionMode: false
+});
+
+router.post('/create-payment-intent', async (req, res, next) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 19400,
+    currency: 'jpy',
+    statement_descriptor: 'My Store',
+    confirm: false,
+    payment_method_types: ['card']
+  });
+
+  res.json({
+    clientSecret: paymentIntent.client_secret
+  });
+});
+
+router.post('/api/create-paypay', async (req, res, next) => {
+  console.log(req.body);
+
+  const paymentId = uuid.v4();
+  const payload = {
+      merchantPaymentId: paymentId,
+      amount: { "amount": 19140, "currency": "JPY" },
+      codeType: 'ORDER_QR',
+      orderItems: null,
+      redirectUrl: 'https://yu-demo.tunnel.stripe.me/thanks.html',
+      redirectType: 'WEB_LINK',
+  };
+  console.log(payload);
+
+  PAYPAY.QRCodeCreate(payload, (ppResonse) => {
+    const body = ppResonse.BODY;
+    console.log(JSON.stringify(ppResonse));
+    console.log(ppResonse.STATUS, body.data);
+    console.log(body.data.url);
+    res.send({ url: body.data.url});
+  });
+
+
+});
+
+//module.exports = router;
+const app = express();
+
+app.use('/', router);
+app.use(express.static(path.join(__dirname, './public')));
+
+// port where app is served
+app.listen(3000, () => {
+    console.log('The web server has started on http://localhost:3000');
+});
